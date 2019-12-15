1e0debb15e8202d1318e06f24a4372866c1d667c
abbiyukd <38352511+abbiyukd@users.noreply.github.com>
2019-12-15 13:04:42 +0700

    Add files via upload

---
+++ src/ta_abairagency/ta_SetGet.java
@@ -0,0 +1,19 @@
+package ta_abairagency;
+
+public class ta_SetGet {
+    private int waktu = 0;
+    private int jarak = 0;
+    
+    public void setWaktu(int waktu) {
+        this.waktu = waktu;
+    }
+    public void setJarak(int jarak) {
+        this.jarak = jarak;
+    }
+    public int getWaktu(){
+        return waktu;
+    }
+    public int getJarak(){
+        return jarak;
+    }
+}
